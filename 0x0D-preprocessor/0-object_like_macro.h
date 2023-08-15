9f68c125c44fbc84d759f6d2292f94f3ff09c661
Lyneotema <lyneotema752@gmail.com>
2023-08-15 10:33:09 -0700

    macro definition replacing SIZE with 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: 0-object_like_macro.h
+ * Desc: Header file describing macro SIZE
+ */
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+0x0D. C - Preprocessor
+Preprocessor - Compiler - Assembler - Linker : Steps of compilation
+Uses of preprocessors: directives,constants,macros
