4b0d1dffeceb3a1897b1bfccfa6416543c8e0b2d
Lyneotema <lyneotema752@gmail.com>
2023-08-15 13:22:21 -0700

    computes absolute value of x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * File: 3-function_like_macro.h
+ * @x: Input
+ * Desc: header file computing absolute value
+ */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
+
+
---
+++ 0x0D-preprocessor/function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File: 3-function_like_macro.h
+ * @x: Input
+ * Desc:Header computing absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
